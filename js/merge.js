f9afe4d7ea66facd857c8ecfc99d3686de6b7dcf
StevenLuo <532265997@qq.com>
2015-08-28 13:49:27 +0800

    重构代码

--- js/merge.js
+++ js/merge.js
@@ -1,75 +1,114 @@
-var Merge = function() {
-	this.padding = 5;
-	this.fontSize = 14;
-	this.lineHeight = 20;
-}
-Merge.prototype.getImage = function(img, words, callback) {
-	var _this = this,
-		myImg = new Image();
-	myImg.src = img;
-	myImg.onload = function() {
-		var ratio = this.width / 800;
-		var width = 800,
-			height = this.height / ratio;
-		var canvas = document.createElement('canvas');
-		canvas.width = width;
-		canvas.height = height;
-		var ctx = canvas.getContext('2d');
-		ctx.drawImage(this, 0, 0, width, height);
-		//绘制矩形背景
-		ctx.fillStyle = 'rgba(0, 0, 0, .7)';
-		var lines = Math.ceil(_this._getStringLength(words) * _this.fontSize / 2 / (width - _this.padding * 2));
-		var wordsHeight = lines * _this.fontSize + _this.padding * 2 + (lines -1) * ( _this.lineHeight - _this.fontSize);
-		ctx.fillRect(0, height - wordsHeight, width, wordsHeight);
-		//填充文字
-		_this._addText(ctx, width, height, words, wordsHeight);
-		callback(canvas.toDataURL("image/jpeg"));
-	}
-}
-Merge.prototype._addText = function(ctx, width, height, words, wordsHeight) {
-	ctx.font = this.fontSize + "px 宋体";
-	ctx.fillStyle = "#fff";
-	ctx.textBaseline = 'top';
-	var text = words;
-	var lineLength = (width - this.padding * 2) / (this.fontSize / 2);
-	for (var i = 0; this._getStringLength(text) > 0; i++) {
-		var index = this._cutString(text, lineLength);
-		ctx.fillText(text.substr(0, index), this.padding, i * this.lineHeight + height - wordsHeight + this.padding);
-		text = text.substr(index);
-	}
-}
-Merge.prototype._getStringLength = function(str) {
-	var len = str.length,
-		trueLen = 0;
-	for (var i = 0; i < len; i++) {
-		if (str.charCodeAt(i) > 128) {
-			trueLen += 2;
-		} else {
-			trueLen += 1;
-		}
-	}
-	return trueLen;
-}
-Merge.prototype._cutString = function(str, lineLength) {
-	var len = str.length,
-		index = len,
-		curLen = 0;
-	for (var i = 0; i < len; i++) {
-		if (str.charCodeAt(i) > 128) {
-			if (curLen + 2 < lineLength) {
-				curLen += 2;
-			} else {
-				index = i;
-				break;
+var Merge = (function() {
+	var defaults = {
+			'imgUrl': '',
+			'words': '',
+			'padding': 5,
+			'fontSize': 14,
+			'lineHeight': 20
+		},
+		options
+
+	var getImage = function(newOptions, callback) {
+			options = _extend(defaults, newOptions)
+
+			if (!options.imgUrl && !options.words) {
+				console.log('参数imgUrl和words不能为空')
+				return
 			}
-		} else {
-			if (curLen + 1 < lineLength) {
-				curLen += 1;
-			} else {
-				index = i;
-				break;
+
+			var _this = this,
+				newImg = new Image()
+
+			newImg.src = options.imgUrl
+			newImg.onload = function() {
+				var ratio = this.width / 800
+				var width = 800,
+					height = this.height / ratio
+				var canvas = document.createElement('canvas')
+				canvas.width = width
+				canvas.height = height
+				var ctx = canvas.getContext('2d')
+				ctx.drawImage(this, 0, 0, width, height)
+					//绘制矩形背景
+				ctx.fillStyle = 'rgba(0, 0, 0, .7)'
+				var lines = Math.ceil(_getStringLength(options.words) * options.fontSize / 2 / (width - options.padding * 2))
+				var wordsHeight = lines * options.fontSize + options.padding * 2 + (lines - 1) * (options.lineHeight - options.fontSize)
+				ctx.fillRect(0, height - wordsHeight, width, wordsHeight)
+					//填充文字
+				_addText(ctx, width, height, wordsHeight)
+
+				if (callback) {
+					callback(canvas.toDataURL("image/jpeg"))
+				}
+			}
+		},
+		_addText = function(ctx, width, height, wordsHeight) {
+			ctx.font = options.fontSize + "px 宋体"
+			ctx.fillStyle = "#fff"
+			ctx.textBaseline = 'top'
+			var text = options.words
+			var lineLength = (width - options.padding * 2) / (options.fontSize / 2)
+			for (var i = 0; _getStringLength(text) > 0; i++) {
+				var index = _cutString(text, lineLength)
+				ctx.fillText(text.substr(0, index), options.padding, i * options.lineHeight + height - wordsHeight + options.padding)
+				text = text.substr(index)
+			}
+		},
+		_getStringLength = function(str) {
+			var len = str.length,
+				trueLen = 0
+			for (var i = 0; i < len; i++) {
+				if (str.charCodeAt(i) > 128) {
+					trueLen += 2
+				} else {
+					trueLen += 1
+				}
+			}
+			return trueLen
+		},
+		_cutString = function(str, lineLength) {
+			var len = str.length,
+				index = len,
+				curLen = 0
+			for (var i = 0; i < len; i++) {
+				if (str.charCodeAt(i) > 128) {
+					if (curLen + 2 < lineLength) {
+						curLen += 2
+					} else {
+						index = i
+						break
+					}
+				} else {
+					if (curLen + 1 < lineLength) {
+						curLen += 1
+					} else {
+						index = i
+						break
+					}
+				}
+			}
+			return index;
+		},
+		_extend = function(defaultOpts, newOpts) {
+			if (typeof newOpts == 'object') {
+
+				if (newOpts instanceof Array)
+					return
+
+				for (var newKey in newOpts) {
+					for (var oldKey in defaultOpts) {
+						if (newKey == oldKey) {
+							defaultOpts[newKey] = newOpts[newKey]
+						} else {
+							defaultOpts[newKey] = newOpts[newKey]
+						}
+					}
+				}
+
+				return defaultOpts
 			}
 		}
+	return {
+		getImage: getImage,
 	}
-	return index;
-}
+}())
